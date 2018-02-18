32684d3805292253d0d6a236f16781f29dc2a40f
medalon <medalon07@gmail.com>
2018-02-18 22:40:32 +0600

    функция youtube search готова

--- bot/youtube.go
+++ bot/youtube.go
@@ -1,17 +1,63 @@
 package bot
 
 import (
-        "fmt"
-     )
-   
-func Search() {
-        fmt.Println("Эта функция ищет ID видео")
-}
+	"flag"
+	"log"
+	"net/http"
 
-func GetVideoUrl() {
-        fmt.Println("Эта функция ищет само видео")
-}
+	"google.golang.org/api/googleapi/transport"
+	"google.golang.org/api/youtube/v3"
+)
 
-func Download() {
-        fmt.Println("Эта функция загружает файл")
+var (
+	query      = flag.String("query", "Google", "Search term")
+	maxResults = flag.Int64("max-results", 1, "Max YouTube results")
+)
+
+const developerKey = "DEVELOPER KEY PASTE HERE"
+
+// Search эта функция возвращает id видеофайла
+// найденного в ютубе (первого совпавщего)
+func Search(searchText string) string {
+
+	*query = searchText
+
+	flag.Parse()
+
+	client := &http.Client{
+		Transport: &transport.APIKey{Key: developerKey},
+	}
+
+	service, err := youtube.New(client)
+	if err != nil {
+		log.Fatalf("Error creating new YouTube client: %v", err)
+	}
+
+	// Make the API call to YouTube.
+	call := service.Search.List("id,snippet").
+		Q(*query).
+		MaxResults(*maxResults)
+	response, err := call.Do()
+	if err != nil {
+		log.Fatalf("Error making search API call: %v", err)
+	}
+
+	// Group video results in separate lists.
+	videos := make(map[string]string)
+
+	// Iterate through each item and add it to the correct list.
+	for _, item := range response.Items {
+		switch item.Id.Kind {
+		case "youtube#video":
+			videos[item.Id.VideoId] = item.Snippet.Title
+		}
+	}
+
+	keys := make([]string, 0, len(videos))
+	for k := range videos {
+		keys = append(keys, k)
+	}
+
+	return keys[0]
+
 }
