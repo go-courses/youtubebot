dc7a9818f4aa5de115ab0a503525efec24710dc9
Anatoliy Fedorenko <fedorenko.tolik@gmail.com>
2018-02-22 13:30:54 +0600

    добавил комментарии

--- bot/bot.go
+++ bot/bot.go
@@ -11,13 +11,25 @@ func Start() {
 			continue
 		}
 
+		// получаем любой текст от пользователя
 		text := update.Message.Text
+
+		// используем Youtube API чтобы найти самый подходящий запрос под этот текст
 		id := Search(text)
+
+		// чтобы пользователь понимал, что бот работает, выводим в чат сообщение
 		SendMsg(update, bot, "Начал поиск")
+
+		// пока что чат бот выдаёт найденное видео в виде полной ссылки и отправляет его
 		url := "https://www.youtube.com/watch?v=" + id
 		SendMsg(update, bot, url)
-		//url, _ := GetDownloadUrl(id)
-		SendAudio(update, bot, "files/music.mp3")
 
+		// здесь надо написать код, который скачивает видео и конвертирует его в mp3 например
+
+		// для пользователя, чтобы знал, что бот работает.
+		SendMsg(update, bot, "Начал конвертацию")
+
+		// в конце загружается готовое аудио через указанный путь.
+		SendAudio(update, bot, "files/music.mp3")
 	}
 }
