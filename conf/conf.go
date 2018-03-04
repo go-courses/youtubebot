8ccd27af7ba47f0592cd2e2882a17e5a13f99bf3
Gen1us2k <minkin.andrew@gmail.com>
2018-03-04 14:29:24 +0600

    Добавил пакет конфигурации приложенрия я с тестами

---
+++ conf/conf.go
@@ -0,0 +1,16 @@
+package conf
+
+import "github.com/kelseyhightower/envconfig"
+
+// BotConfig ...
+type BotConfig struct {
+	TelegramToken       string `envconfig:"TELEGRAM_TOKEN"`
+	YoutubeDeveloperKey string
+}
+
+// GetConfig ...
+func GetConfig() (BotConfig, error) {
+	var c BotConfig
+	err := envconfig.Process("bot", &c)
+	return c, err
+}
